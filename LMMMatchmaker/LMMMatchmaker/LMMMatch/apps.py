a573ee16df7cfcde8198fb141acb3d471c521687
Waleed <108493771+Waamer@users.noreply.github.com>
2023-04-01 01:17:31 -0400

    Register page fixed

---
+++ LMMMatchmaker/LMMMatchmaker/LMMMatch/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LmmmatchConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'LMMMatch'
--- LMMMatchmaker/users/forms.py
+++ LMMMatchmaker/users/forms.py
@@ -3,11 +3,15 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=50,required=True)
-    last_name = forms.CharField(max_length=50,required=True)
     
-
+    error_css_class = 'errorClass'
+    
+    email = forms.EmailField(widget= forms.TextInput(attrs={'placeholder':'Enter your Email'}), label='')
+    username = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Enter a new Username'}), label='')
+    first_name = forms.CharField(max_length=50,required=True, widget= forms.TextInput(attrs={'placeholder':'Enter your first name'}), label='')
+    last_name = forms.CharField(max_length=50,required=True, widget= forms.TextInput(attrs={'placeholder':'Enter your last name'}), label='')
+    password1 = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Enter a new Password', 'type' : 'password'}), label='')
+    password2 = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Confirm your password', 'type' : 'password'}), label='')
 
     class Meta:
         model = User
